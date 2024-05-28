9dc7ae362ae21ec38f2463964a37dbcbc21e8a6d
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-28 18:32:25 -0300

    tela inicio WIP

---
+++ js/app.js
@@ -0,0 +1,3 @@
+const navbar = {
+    current: "3",
+};
--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,16 @@
-const navbar = {
-    current: 2
+function renderNavbarActive() {
+    const indicators = document.querySelectorAll(".indicator");
+    for (const id of indicators) {
+        if (id.dataset.id === navbar.current) {
+            id.classList.add("active");
+        } else {
+            id.classList.remove("active");
+        }
+    }
 }
 
+function onLoad() {
+    renderNavbarActive();
+}
+
+document.addEventListener("DOMContentLoaded", onLoad);
